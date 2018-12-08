4d222154b06f518bb574f093af09a1d4b3e8a494
DESKTOP-CF191QO\Andreas <andriou.p@live.com>
2018-12-08 21:44:00 +0200

    rm scrapy

--- app.py
+++ app.py
@@ -1,39 +1,9 @@
 from flask import Flask, render_template, request
 from bot import Bot
 from string import Template
-from scrapy.crawler import CrawlerProcess
-from spiders.ouc_modules import OucModulesSpider
-import os
-import json
+
 app = Flask(__name__)
-current_path = os.path.dirname(os.path.realpath(__file__))
-module_json = "modules.json"
-module_yml = os.path.join(current_path, "data/cos/modules.yml")
 
-if os.path.exists(module_json):
-    os.remove(module_json)
-
-crawler = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    'FEED_FORMAT': 'json',
-    'FEED_URI': "cos-chatterbot/" + module_json
-})
-
-crawler.crawl(OucModulesSpider)
-crawler.start()
-
-if os.path.exists(module_yml):
-    os.remove(module_yml)
-
-with open(module_yml, "w") as f:
-    f.write('categories:' + '\n')
-    f.write('- modules' + '\n')
-    f.write('conversations:' + '\n')
-    with open(os.path.join(current_path, module_json)) as mj:
-        for item in json.load(mj):
-            f.write('- - ' + item['code']+'\n')
-            f.write('  - ' + '^' + item['code'] + '\n')
-f.close()
 cos_bot = Bot(train=True)
 
 
